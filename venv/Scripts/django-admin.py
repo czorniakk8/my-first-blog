3591ad4f77d431dd0937e4c0d1b400ff56357b85
czorniakk8 <czorniakk8@o2.pl>
2021-01-10 11:45:03 +0100

    My first Django App and first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\xd\python\django\projekty\firstprojectlearn\code\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
